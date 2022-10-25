72d5931053f0d5a2a341b6c66b18c138e961b269
Sai <rsaisankalp@gmail.com>
2022-10-25 14:05:19 +0530

    update

--- app-multiple-account.js
+++ app-multiple-account.js
@@ -1,4 +1,5 @@
 const { Client, MessageMedia, LocalAuth } = require('whatsapp-web.js');
+const { body, validationResult } = require('express-validator');
 const express = require('express');
 const socketIO = require('socket.io');
 const qrcode = require('qrcode');
@@ -12,6 +13,7 @@ const port = process.env.PORT || 8000;
 const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
+let mediaStore = {}
 
 app.use(express.json());
 app.use(express.urlencoded({
@@ -191,10 +193,10 @@ io.on('connection', function(socket) {
 app.post('/send-message', async (req, res) => {
   console.log(req);
 
-  const sender = req.body.sender;
+  
   const number = phoneNumberFormatter(req.body.number);
   const message = req.body.message;
-
+  const sender = req.body.sender;
   const client = sessions.find(sess => sess.id == sender)?.client;
 
   // Make sure the sender is exists & ready
@@ -212,6 +214,7 @@ app.post('/send-message', async (req, res) => {
    * Please check app.js for more validations example
    * You can add the same here!
    */
+  
   const isRegisteredNumber = await client.isRegisteredUser(number);
 
   if (!isRegisteredNumber) {
@@ -220,7 +223,7 @@ app.post('/send-message', async (req, res) => {
       message: 'The number is not registered'
     });
   }
-
+  
   client.sendMessage(number, message).then(response => {
     res.status(200).json({
       status: true,
@@ -233,6 +236,354 @@ app.post('/send-message', async (req, res) => {
     });
   });
 });
+
+
+// Send media
+app.post('/send-media', async (req, res) => {
+  const number = phoneNumberFormatter(req.body.number);
+  const caption = req.body.caption;
+  const fileUrl = req.body.file;
+  const sender = req.body.sender;
+  const client = sessions.find(sess => sess.id == sender)?.client;
+
+  // Make sure the sender is exists & ready
+  if (!client) {
+    return res.status(422).json({
+      status: false,
+      message: `The sender: ${sender} is not found!`
+    })
+  }
+
+  // const media = MessageMedia.fromFilePath('./image-example.png');
+  // const file = req.files.file;
+  // const media = new MessageMedia(file.mimetype, file.data.toString('base64'), file.name);
+  let mimetype;
+  const attachment = await axios.get(fileUrl, {
+    responseType: 'arraybuffer'
+  }).then(response => {
+    mimetype = response.headers['content-type'];
+    return response.data.toString('base64');
+  });
+
+  const media = new MessageMedia(mimetype, attachment, 'Media');
+
+  client.sendMessage(number, media, {
+    caption: caption
+  }).then(response => {
+    res.status(200).json({
+      status: true,
+      response: response
+    });
+  }).catch(err => {
+    res.status(500).json({
+      status: false,
+      response: err
+    });
+  });
+});
+
+const findGroupByName = async function(name) {
+  const group = await client.getChats().then(chats => {
+    return chats.find(chat => 
+      chat.isGroup && chat.name.toLowerCase() == name.toLowerCase()
+    );
+  });
+  return group;
+}
+
+// Send message to group
+// You can use chatID or group name, yea!
+app.post('/send-group-message', [
+  body('id').custom((value, { req }) => {
+    if (!value && !req.body.name) {
+      throw new Error('Invalid value, you can use `id` or `name`');
+    }
+    return true;
+  }),
+  body('message').notEmpty(),
+], async (req, res) => {
+  const errors = validationResult(req).formatWith(({
+    msg
+  }) => {
+    return msg;
+  });
+
+  if (!errors.isEmpty()) {
+    return res.status(422).json({
+      status: false,
+      message: errors.mapped()
+    });
+  }
+
+  let chatId = req.body.id;
+  const groupName = req.body.name;
+  const message = req.body.message;
+  const sender = req.body.sender;
+  try {
+  const client = sessions.find(sess => sess.id == sender)?.client;
+
+  // Make sure the sender is exists & ready
+  if (!client) {
+    return res.status(422).json({
+      status: false,
+      message: `The sender: ${sender} is not found!`
+    })
+  }
+
+
+  // Find the group by name
+  if (!chatId) {
+    const group = await findGroupByName(groupName);
+    if (!group) {
+      return res.status(422).json({
+        status: false,
+        message: 'No group found with name: ' + groupName
+      });
+    }
+    chatId = group.id._serialized;
+  }
+
+  client.sendMessage(chatId, message).then(response => {
+    res.status(200).json({
+      status: true,
+      response: response
+    });
+  }).catch(err => {
+    res.status(500).json({
+      status: false,
+      response: err
+    });
+  });
+  } catch(e) {
+    res.status(500).json({
+      status: false,
+      response: err
+    });
+  }
+});
+
+app.post('/get-status', async (req,res) => {
+  
+  const sender = req.body.sender;
+  const client = sessions.find(sess => sess.id == sender)?.client;
+
+  // Make sure the sender is exists & ready
+  if (!client) {
+    return res.status(422).json({
+      status: false,
+      message: `The sender: ${sender} is not found!`
+    })
+  }
+  try {
+  const savedSessions = getSessionsFile();
+  const sessionIndex = savedSessions.findIndex(sess => sess.id == sender);
+  let status = savedSessions[sessionIndex].ready 
+  res.status(200).json({
+    status: status,
+    response: "connected"
+  });
+  } catch(e) {
+    res.status(500).json({
+      status: false,
+      response: "not connected"
+    });
+  }
+});
+
+app.post('/get-groups', async (req, res) => {
+  const sender = req.body.sender;
+  try {
+  const client = sessions.find(sess => sess.id == sender)?.client;
+
+  // Make sure the sender is exists & ready
+  if (!client) {
+    return res.status(422).json({
+      status: false,
+      message: `The sender: ${sender} is not found!`
+    })
+  }
+
+  const groups = await client.getChats().then(chats => {
+    return chats.find(chat => 
+      chat.isGroup 
+    );
+  });
+  console.log("groups:"+JSON.stringify(client.getChats()))
+  res.status(200).json({
+    status: true,
+    response: groups
+  });
+  } catch(e) {
+    res.status(500).json({
+      status: false,
+      response: "failed"
+    });
+  }
+
+
+});
+
+app.post('/send-group-message-media', [
+  body('id').custom((value, { req }) => {
+    if (!value && !req.body.name) {
+      console.log("print throw");
+      throw new Error('Invalid value, you can use `id` or `name`');
+    }
+    return true;
+  }),
+  body('message').notEmpty(),
+], async (req, res) => {
+  // const errors = validationResult(req).formatWith(({
+  //   msg
+  // }) => {
+  //   console.log("print msg")
+  //   return msg;
+  // });
+
+  // if (!errors.isEmpty()) {
+  //   console.log("print errors")
+  //   return res.status(422).json({
+  //     status: false,
+  //     message: errors.mapped()
+  //   });
+  // }
+
+  /////
+  const caption = req.body.caption;
+  const fileUrl = req.body.file;
+  let chatId = req.body.id;
+  const sender = req.body.sender;
+  const groupName = req.body.name;
+  try {
+  const client = sessions.find(sess => sess.id == sender)?.client;
+
+  // Make sure the sender is exists & ready
+  if (!client) {
+    return res.status(422).json({
+      status: false,
+      message: `The sender: ${sender} is not found!`
+    })
+  }
+
+  // const media = MessageMedia.fromFilePath('./image-example.png');
+  // const file = req.files.file;
+  // const media = new MessageMedia(file.mimetype, file.data.toString('base64'), file.name);
+  let attachment = null;
+  if(mediaStore[fileUrl] != null) {
+    attachment = mediaStore[fileUrl]
+  } else {
+    let mimetype;
+    attachment = await axios.get(fileUrl, {
+      responseType: 'arraybuffer'
+    }).then(response => {
+      mimetype = response.headers['content-type'];
+      return response.data.toString('base64');
+    });
+  }
+  console.log("attachment: "+attachment)
+
+  const media = new MessageMedia(mimetype, attachment, 'Media');
+
+
+  // Find the group by name
+  if (!chatId) {
+    const group = await findGroupByName(groupName);
+    if (!group) {
+      return res.status(422).json({
+        status: false,
+        message: 'No group found with name: ' + groupName
+      });
+    }
+    chatId = group.id._serialized;
+  }
+  console.log("print chat")
+  client.sendMessage(chatId, media, {
+    caption: caption
+  }).then(response => {
+    res.status(200).json({
+      status: true,
+      response: response
+    });
+  }).catch(err => {
+    console.log("error"+err)
+    res.status(500).json({
+      status: false,
+      response: err
+    });
+  });
+} catch(e) {
+  res.status(500).json({
+    status: false,
+    response: "failed"
+  });
+}
+  
+
+
+});
+
+app.post('/free-media-store', async (req,res) => {
+  mediaStore = {}
+  res.status(200).json({
+    status: true,
+    response: "freed"
+  });
+})
+
+// Clearing message on spesific chat
+app.post('/clear-message', [
+  body('number').notEmpty(),
+], async (req, res) => {
+  const errors = validationResult(req).formatWith(({
+    msg
+  }) => {
+    return msg;
+  });
+
+  if (!errors.isEmpty()) {
+    return res.status(422).json({
+      status: false,
+      message: errors.mapped()
+    });
+  }
+
+  const number = phoneNumberFormatter(req.body.number);
+  const sender = req.body.sender;
+  const client = sessions.find(sess => sess.id == sender)?.client;
+
+  // Make sure the sender is exists & ready
+  if (!client) {
+    return res.status(422).json({
+      status: false,
+      message: `The sender: ${sender} is not found!`
+    })
+  }
+
+
+  const isRegisteredNumber = await checkRegisteredNumber(number);
+
+  if (!isRegisteredNumber) {
+    return res.status(422).json({
+      status: false,
+      message: 'The number is not registered'
+    });
+  }
+
+  const chat = await client.getChatById(number);
+  
+  chat.clearMessages().then(status => {
+    res.status(200).json({
+      status: true,
+      response: status
+    });
+  }).catch(err => {
+    res.status(500).json({
+      status: false,
+      response: err
+    });
+  })
+});
+
 
 server.listen(port, function() {
   console.log('App running on *: ' + port);
